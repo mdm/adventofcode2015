cb564e393545e0fb8ee092d9ec37245ae9ada6dd
Marc Dominik Migge <marcmigge@gmx.net>
2021-01-24 13:12:59 +0100

    Finish day 3

---
+++ day03/src/Lib.hs
@@ -0,0 +1,26 @@
+module Lib
+    ( part1
+    , part2
+    ) where
+
+import Data.List(nub)
+
+translate :: Char -> (Int, Int)
+translate '<' = (-1, 0)
+translate '>' = (1, 0)
+translate '^' = (0, -1)
+translate 'v' = (0, 1)
+
+move (x, y) (a, b) = (x + a, y + b)
+
+partition = partition' [] []
+    where partition' ys zs [] = (reverse ys, reverse zs)
+          partition' ys zs (x:xs) = partition' (x:zs) ys xs
+
+part1 :: String -> String
+part1 = show . length . nub . scanl move (0, 0) . map translate
+
+part2 :: String -> String
+part2 = show . length . nub . combine . partition . map translate
+    where combine (xs, ys) = scanl move (0, 0) xs ++ scanl move (0, 0) ys
+
