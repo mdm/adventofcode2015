18d86a68adcdc893f30cd4ca78f93b515bee705b
Marc Dominik Migge <marcmigge@gmx.net>
2021-03-07 17:34:27 +0100

    Improve memory efficiency

--- day19/src/Lib.hs
+++ day19/src/Lib.hs
@@ -7,8 +7,10 @@ import Text.Parsec.String (Parser)
 import Text.Parsec (string, parse)
 import Text.Parsec.Char (letter)
 import Text.Parsec.Combinator (many1)
-import Data.List (nub, partition, isPrefixOf, isSubsequenceOf, tails)
+import Data.List (nub, partition, isPrefixOf, isSubsequenceOf, tails, sortBy)
 import Data.Tuple (swap)
+import Data.Maybe (mapMaybe)
+import Control.Monad (msum)
 
 
 data Token = Terminal String | NonTerminal String
@@ -54,7 +56,26 @@ tokenize rules [] = []
 tokenize rules string = token:tokenize rules rest
     where (token, rest) = nextToken rules string
 
+rhsLength:: (String, String) -> (String, String) -> Ordering 
+rhsLength (_, a) (_, b) = compare (length b) (length a)
+
+choice :: [(String, String)] -> [Token] -> [Token] -> Maybe Int
+choice rules ((NonTerminal x):xs) ys | length xs < length ys = if null options then Nothing else Just $ 1 + head options
+                                     | otherwise = Nothing
+    where matchingRules = filter ((==x) . fst) rules
+          applyRule (_, zs) = parseMedicine rules (tokenize rules zs ++ xs) ys
+          options = mapMaybe applyRule matchingRules
+
+parseMedicine :: [(String, String)] -> [Token] -> [Token] -> Maybe Int
+parseMedicine rules [] ys = if null ys then Just 0 else Nothing
+parseMedicine rules _ [] = Nothing
+parseMedicine rules ((Terminal x):xs) ((Terminal y):ys) = if x == y then parseMedicine rules xs ys else Nothing
+parseMedicine rules ((Terminal x):xs) ((NonTerminal y):ys) = Nothing
+parseMedicine rules start@((NonTerminal x):xs) target@((Terminal y):ys) = choice rules start target
+parseMedicine rules start@((NonTerminal x):xs) target@((NonTerminal y):ys) = msum [if x == y then parseMedicine rules xs ys else Nothing, choice rules start target]
+
 part2 :: String -> String
-part2 input = show $ tokenize parsedRules medicine
+part2 input = show $ parseMedicine (sortBy rhsLength parsedRules) (tokenize parsedRules "e") (tokenize parsedRules medicine)
+-- part2 input = show $ sortBy rhsLength parsedRules
     where (medicine:_:rules) = reverse . lines $ input
           parsedRules = map (either undefined id . parse parseReplacement "") rules
