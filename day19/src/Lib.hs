b37204e5b60d785f957196854ce0462b4a6962fe
Marc Dominik Migge <marcmigge@gmx.net>
2021-04-24 16:11:10 +0200

    Finish day 19, part 2 - finally!

--- day19/src/Lib.hs
+++ day19/src/Lib.hs
@@ -16,9 +16,6 @@ import Data.Foldable (minimumBy, maximumBy)
 import Data.Char (isAsciiUpper)
 
 
-data Token = Terminal String | NonTerminal String
-    deriving (Show, Eq)
-
 parseReplacement :: Parser (String, String)
 parseReplacement = do
                      input <- many1 letter
@@ -36,101 +33,23 @@ part1 :: String -> String
 part1 input = show . length . nub . filter (/=start) . concatMap (replace start . either undefined id . parse parseReplacement "") $ rules
     where (start:_:rules) = reverse . lines $ input
 
-allReductions :: [(String, String)] -> [Token] -> [Token] -> [[Token]]
-allReductions _ _ [] = []
-allReductions _ _ [x] = []
-allReductions rules acc ((NonTerminal x1):(NonTerminal x2):tokens) = map (\(lhs, rhs) -> acc ++ NonTerminal lhs:tokens) (matchingRules (x1 ++ x2)) ++ allReductions rules (acc ++ [NonTerminal x1]) (NonTerminal x2:tokens)
-    where matchingRules rhs = filter ((==rhs) . snd) rules
-
-parsePure :: [(String, String)] -> [Token] -> [(Int, [Token])]
-parsePure rules [x] = [(0, [x])]
-parsePure rules input = nub $ concatMap (map plusOne . parsePure rules) (allReductions rules [] input)
-    where plusOne (depth, x) = (depth + 1, x)
-
-replace2 :: [Token] -> ([Token], [Token]) -> [[Token]]
-replace2 [] _ = [[]]
-replace2 start@(x:xs) rule@(input, output) | output `isPrefixOf` start = (input ++ rest):map (x:) (replace2 xs rule)
-                                           | otherwise = map (x:) (replace2 xs rule)
-    where rest = drop (length output) start
-
-parsePure2 :: [(String, String)] -> [Token] -> [(Int, [Token])]
-parsePure2 rules [x] = [(0, [x])]
--- parsePure2 rules input = nub $ concatMap (map plusOne . parsePure2 rules) $ filter (/=input) $ concatMap (\r -> trace (show input ++ " " ++ show r) $ replace2 input r) tokenizedRules
-parsePure2 rules input = nub $ concatMap (map plusOne . parsePure2 rules) $ filter (/=input) $ concatMap (replace2 input) tokenizedRules
-    where plusOne (depth, x) = (depth + 1, x)
-          tokenizedRules = zip (map (tokenize rules "" . fst) rules) (map (tokenize rules "" . snd) rules)
-
-withTerminals :: [Token] -> Bool
-withTerminals [] = False
-withTerminals ((Terminal x):xs) = True
-withTerminals ((NonTerminal x):xs) = withTerminals xs
-
-shallowest :: [(Int, [Token])] -> [(Int, [Token])]
-shallowest [] = []
-shallowest candidates = [minimumBy depth candidates]
-    where depth x y = compare (fst x) (fst y)
-
-deepest :: [(Int, [Token])] -> [(Int, [Token])]
-deepest [] = []
-deepest candidates = [maximumBy depth candidates]
-    where depth x y = compare (fst x) (fst y)
-
-prependToken :: [(String, String)] -> String-> [Token] -> [Token]
-prependToken rules acc xs = if null acc then xs else (makeToken $ reverse acc):xs
-    where lefts = map fst rules
-          makeToken ys | ys `elem` lefts = NonTerminal ys
-                       | otherwise = Terminal ys
-
-tokenize :: [(String, String)] -> String -> String -> [Token]
-tokenize rules acc [] = prependToken rules acc []
-tokenize rules acc (x:xs) | isAsciiUpper x && acc /= "C" = prependToken rules acc $ tokenize rules [x] xs
-                          | otherwise = tokenize rules (x:acc) xs
-
-showTokens :: [Token] -> [String]
-showTokens [] = []
-showTokens ((Terminal t):xs) = t:showTokens xs
-showTokens ((NonTerminal t):xs) = t:showTokens xs
 
 prependAcc :: [a] -> [[a]] -> [[a]]
 prependAcc acc xs = if null acc then xs else (reverse acc):xs
 
-combineNested :: [Token] -> Int -> [Token] -> ([Token], [Token])
-combineNested [] _ acc = (reverse acc, [])
-combineNested input count acc | head input == Terminal "Rn" || head input == Terminal "CRn" = combineNested (tail input) (count + 1) ((head input):acc)
-                              | head input == Terminal "Ar" && count == 0 = (reverse (head input:acc), tail input)
-                              | head input == Terminal "Ar" = combineNested (tail input) (count - 1) ((head input):acc)
-                              | otherwise = combineNested (tail input) count ((head input):acc)
+tokenize :: String -> String -> [String]
+tokenize acc [] = prependAcc acc []
+tokenize acc (x:xs) | isAsciiUpper x = prependAcc acc $ tokenize [x] xs
+                    | otherwise = tokenize (x:acc) xs
 
-splitInput :: Bool -> [Token] -> [Token] -> [[Token]]
-splitInput _ acc [] = prependAcc acc []
-splitInput skip acc (x@(Terminal t):xs) | (t == "Rn" || t == "CRn") && not skip = prependAcc (x:acc) $ splitInput True [] xs
-                                        | t == "Rn" || t == "CRn" = splitInput skip (reverse combined ++ (x:acc)) rest
-                                        | t == "Ar" = prependAcc acc $ splitInput False [x] xs
-                                        | otherwise = prependAcc acc $ [x]:splitInput skip [] xs
-    where (combined, rest) =  combineNested xs 0 []
-splitInput skip acc (x@(NonTerminal _):xs) = splitInput skip (x:acc) xs
-
-isTerminal (Terminal _) = True
-isTerminal (NonTerminal _) = False
-
-parsePartial :: [(String, String)] -> [Token] -> (Int, [Token])
-parsePartial rules input | isTerminal (head input) || isTerminal (last input) = (0, input)
-                         | otherwise = parseInput rules input
-
-parseInput :: [(String, String)] -> [Token] -> (Int, [Token])
-parseInput rules input | length parts > 1 = (stepOneDepth + stepTwoDepth, stepTwoOutput)
-                       | otherwise = head . shallowest $ parsePure2 rules $ head parts
-    where parts = splitInput False [] input
-          parsedParts = map (parsePartial rules) parts
-          stepOneOutput = concatMap snd parsedParts
-          stepOneDepth = maximum . map fst $ parsedParts
-          (stepTwoDepth, stepTwoOutput) = head . shallowest $ parsePure2 rules stepOneOutput
+minReductions :: Int -> Int -> Int -> [String] -> [String] -> Int
+minReductions t r _ _ [] = t - 2 * r - 1
+minReductions t r depth acc (x:xs) | x == "Rn" = if depth == 0 then minReductions (t + 1) (r + 1) (depth + 1) [] xs else minReductions t r (depth + 1) (x:acc) xs
+                                   | depth == 1 && x == "Y" = minReductions 0 0 0 [] (reverse acc) + minReductions (t + 2) (r + 1) depth [] xs
+                                   | x == "Ar" = if depth == 1 then minReductions 0 0 0 [] (reverse acc) + minReductions (t + 2) r (depth - 1) [] xs else minReductions t r (depth - 1) (x:acc) xs
+                                   | otherwise = if depth == 0 then minReductions (t + 1) r depth acc xs else minReductions t r depth (x:acc) xs
 
 part2 :: String -> String
--- part2 input = show $ showTokens $ (!!20) $ splitInput False [] $ tokenize parsedRules "" medicine
--- part2 input = show $ map showTokens $ splitInput False [] $ tokenize parsedRules "" medicine
-part2 input = show $ parseInput parsedRules $ tokenize parsedRules "" medicine
--- part2 input = show $ parseInput parsedRules $ tokenize parsedRules "" "TiBSiTh"
--- part2 input = show $ parseInput parsedRules $ tokenize parsedRules "" "TiB"
+part2 input = show $ minReductions 0 0 0 [] $ tokenize "" medicine
     where (medicine:_:rules) = reverse . lines $ input
           parsedRules = map (either undefined id . parse parseReplacement "") rules
